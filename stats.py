87b8127390294d1eaf75b8d69cb9750595f45156
mehdi cherti <mehdi@cherti.name>
2016-11-03 06:20:16 +0100

    update

--- stats.py
+++ stats.py
@@ -6,11 +6,9 @@ from likelihood_estimation_parzen import ll_parzen
 import numpy as np
 import sys
 sys.path.append('/home/mcherti/work/code/feature_generation')
-from tools.compute.genstats import compute_out_of_the_box_classification, out_of_the_box_names, out_of_the_box_models
+from tools.compute.genstats import compute_out_of_the_box_classification, out_of_the_box_names, out_of_the_box_models, compute_parzen_letters, compute_parzen_digits
 import pprint
-data = mnist.load(which='test')
-mnist_test_X = data['test']['X'] / 255.
-mnist_test_X = mnist_test_X.astype(np.float32)
+import time
 
 def flatten(x):
     return x.reshape((x.shape[0], -1))
@@ -18,13 +16,15 @@ def flatten(x):
 def compute_stats(j):
     print('Starting {}...'.format(j['summary']))
     st = {}
-    stat_funcs = [parzen, out_of_the_box]
+    stat_funcs = [parzen_digits, parzen_letters, out_of_the_box]
     stats = j.get('stats', {})
     if not stats:
         stats = {}
+    t = time.time()
     for func in stat_funcs:
         st = func(j)
         stats.update(st)
+    print(time.time() - t)
     print('Finished {}'.format(j['summary']))
     #pprint.pprint(stats, indent=2)
     return stats
@@ -35,24 +35,25 @@ def has(j, stat):
     else:
         return False
 
-def parzen(j):
-    if has(j, 'll_parzen_mean'):
+def parzen_digits(j):
+    if has(j, 'parzen_digits'):
         return {}
-    samples = joblib.load(os.path.join('jobs', j['summary'], 'gen.npz'))
-    samples = samples[0:10000]
-    sigma = 0.23
-    ll = ll_parzen(sigma=sigma, samples=flatten(samples), test_X=flatten(mnist_test_X), batch_size=10)
-    ll = np.array(ll)
-    return {'ll_parzen_mean': float(ll.mean()), 'll_parzen_std': float(ll.std() / (100.)) }
+    folder = 'jobs/{}'.format(j['summary'])
+    link(folder)
+    return {'parzen_digits': compute_parzen_digits(folder, nb_samples=10000, batch_size=10)}
+
+def parzen_letters(j):
+    if has(j, 'parzen_letters'):
+        return {}
+    folder = 'jobs/{}'.format(j['summary'])
+    link(folder)
+    return {'parzen_letters': compute_parzen_letters(folder, nb_samples=10000, batch_size=10)}
 
 def out_of_the_box(j):
-    #if has(j, 'out_of_the_box_classification'):
-    #    return {}
+    if has(j, 'out_of_the_box_classification'):
+        return {}
     folder = os.path.join('jobs', j['summary'])
-    try:
-        os.link(folder + '/gen.npz', folder + '/images.npz')
-    except OSError:
-        pass
+    link(folder)
     names = out_of_the_box_names
     models = out_of_the_box_models
     models = ['../../../feature_generation/' + m for m in models]
@@ -60,6 +61,12 @@ def out_of_the_box(j):
     for model_name, model_folder in zip(names, models):
         stat[model_name] = compute_out_of_the_box_classification(folder, model_name, model_folder, nb_samples=1000)
     return {'out_of_the_box_classification': stat}
+
+def link(folder):
+    try:
+        os.link(folder + '/gen.npz', folder + '/images.npz')
+    except OSError:
+        pass
 
 if __name__ == '__main__':
     from lightjob.cli import load_db
